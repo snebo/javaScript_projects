68bf7353160ae846637680f21e4551749a061f67
Stefano <30480337+snebo@users.noreply.github.com>
2024-12-09 06:49:57 +0100

    add task tracker changes

--- task_tracker/task_tracker_modular.js
+++ task_tracker/task_tracker_modular.js
@@ -9,6 +9,48 @@
  * This should use only objects and make sure that the code is modular, efficients, KISS, DRY and non dependent
  */
 
+var Task = (function () {
+	var task = [];
+
+	//cache dom
+	var $taskContainer = $('#tasks_list');
+	var $template = $('#task_template').html();
+	var el = $('#task_module');
+	var $ul = el.find('ul');
+	var $input = $('#input_box');
+	var $button = $('#add_task_btn');
+
+	//bind events
+	$button.on('click', addTask);
+	$ul.delegate('.del', 'click', deleteTask);
+	function render() {
+		var data = { task: task };
+		const rendered = Mustache.render($template, data);
+		$taskContainer.html(rendered);
+	}
+	function addTask(value) {
+		var name = typeof value === 'string' ? value : $input.val();
+		task.push(name);
+		$input.val('');
+		render();
+	}
+	function deleteTask(event) {
+		if (typeof event === 'number') {
+			var index = event;
+		} else {
+			var remove = $(event.target).closest('li');
+			var index = $ul.find('li').index(remove);
+		}
+		task.splice(index, 1);
+		render();
+	}
+
+	return {
+		addTask: addTask,
+		deleteTask: deleteTask,
+	};
+})();
+
 // modular approach. works, but exposes more thean it has to
 // var task = {
 // 	task: [],
@@ -51,48 +93,3 @@
 // document.addEventListener('DOMContentLoaded', function () {
 // 	task.init();
 // });
-
-// document.addEventListener('DOMContentLoaded', function () {
-
-// });
-var Task = (function () {
-	var task = [];
-
-	//cache dom
-	var $taskContainer = $('#tasks_list');
-	var $template = $('#task_template').html();
-	var el = $('#task_module');
-	var $ul = el.find('ul');
-	var $input = $('#input_box');
-	var $button = $('#add_task_btn');
-
-	//bind events
-	$button.on('click', addTask);
-	$ul.delegate('.del', 'click', deleteTask);
-	function render() {
-		var data = { task: task };
-		const rendered = Mustache.render($template, data);
-		$taskContainer.html(rendered);
-	}
-	function addTask(value) {
-		var name = typeof value === 'string' ? value : $input.val();
-		task.push(name);
-		$input.val('');
-		render();
-	}
-	function deleteTask(event) {
-		if (typeof event === 'number') {
-			var index = event;
-		} else {
-			var remove = $(event.target).closest('li');
-			var index = $ul.find('li').index(remove);
-		}
-		task.splice(index, 1);
-		render();
-	}
-
-	return {
-		addTask: addTask,
-		deleteTask: deleteTask,
-	};
-})();
